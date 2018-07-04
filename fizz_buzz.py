7b02bd9eb8046c519405850f66a7dba1d7861149
matchey <19305109+matchey@users.noreply.github.com>
2018-07-04 13:28:51 +0900

    update

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,58 +1,48 @@
 # -*- coding:utf-8 -*-
-import sys
 
 class FizzBuzz:
-    """
-    　 num:指定の数
-    result:指定の数の倍数なら、この文字列を吐き出す
-
-    使い方:
-
-    第一引数がnumに格納され、第二引数がresultに格納される。
-    >>> test = FizzBuzz(3,"Fizz")
-
-    また、is_fizzbuzzメソッドを使うことにより、引数xがnumで割りきれる場合に、resultの文字列を返してくれる。
+    def __init__(self, rules={}):
+        self.rules = rules # {num:string, ...}
+        # self.rules = sorted(rules.items()) # {num:string, ...}
     
-    >>> test.is_fizzbuzz(1)
-    ''
-    >>> test.is_fizzbuzz(2)
-    ''
-    >>> test.is_fizzbuzz(3)
-    'fizz'
-    """
-    def __init__(self,num,string):
-        self.num = num
-        self.result = string
-    
-    def is_fizzbuzz(self,x):
-        " 引数xがnumの倍数なら、resultの文字列を返す "
-        if x % self.num == 0:
-            return self.result
-        else:
-            return ""
+    def fizzbuzz(self, x):
+        # if x % self.num == 0:
+        #     return self.rules[x]
+        # else:
+        #     return ""
+        result = [ i[1] for i in sorted(self.rules.items()) if x % i[0] == 0]
 
+    def add_rule(self, num, string):
+        self.rules[num] = string # numの倍数の時stringを返す
+        # self.rules = sorted(self.rules.items())
+
+    def show(self, count): # count: 上限値
+        # fizzbuzz = [str(i) for i in range(1, count + 1)]
+        fizzbuzz = list(map(str, [ i for i in range(1, count + 1)]))
+
+        print "\n".join(fizzbuzz)
 
 
 def main():
-    "3の倍数ならFizz、5の倍数ならBuzz、両方とも当てはまるならFizzBuzzを返す"
-    argv = sys.argv
-    #もし引数が無ければ、Usageを表示して終了
-    if len(argv) < 2:
-        print "usage:python fizzbuzz.py [number]"
-        exit()
+    # "3の倍数ならFizz、5の倍数ならBuzz、両方とも当てはまるならFizzBuzzを返す
 
-    fizz = FizzBuzz(3,"fizz")
-    buzz = FizzBuzz(5,"buzz")
+    # fizz = FizzBuzz(3, "Fizz")
+    # buzz = FizzBuzz(5, "Buzz")
+    #
+    # for i in range(1, 31):
+    #     result_string = ""
+    #     result_string += fizz.is_fizzbuzz(i)
+    #     result_string += buzz.is_fizzbuzz(i)
+    #     
+    #     if result_string == "":
+    #         print i
+    #     else:
+    #         print result_string
 
-    for i in range(1,int(argv[1]) + 1):
-        result_string = ""
-        result_string += fizz.is_fizzbuzz(i)
-        result_string += buzz.is_fizzbuzz(i)
-        
-        if result_string == "":
-            print i
-        else:
-            print result_string
+    fizzbuzz = FizzBuzz({3:"Fizz", 5:"Buzz"});
+    print fizzbuzz.rules
+    fizzbuzz.show(31)
+    print fizzbuzz.fizzbuzz(6)
 
 
 if __name__ == "__main__":main()
