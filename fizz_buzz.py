aaca67fba7f880e1ac96d465016e1e187ab14775
matchey <19305109+matchey@users.noreply.github.com>
2018-07-04 17:03:57 +0900

    created fizzbuzz

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -5,44 +5,21 @@ class FizzBuzz:
         self.rules = rules # {num:string, ...}
         # self.rules = sorted(rules.items()) # {num:string, ...}
     
-    def fizzbuzz(self, x):
-        # if x % self.num == 0:
-        #     return self.rules[x]
-        # else:
-        #     return ""
-        result = [ i[1] for i in sorted(self.rules.items()) if x % i[0] == 0]
-
     def add_rule(self, num, string):
         self.rules[num] = string # numの倍数の時stringを返す
-        # self.rules = sorted(self.rules.items())
 
     def show(self, count): # count: 上限値
-        # fizzbuzz = [str(i) for i in range(1, count + 1)]
-        fizzbuzz = list(map(str, [ i for i in range(1, count + 1)]))
-
+        fizzbuzz = list(map(self._fizzbuzz, [ i for i in range(1, count + 1)]))
         print "\n".join(fizzbuzz)
 
+    def _fizzbuzz(self, x):
+        result = [ i[1] for i in sorted(self.rules.items()) if x % i[0] == 0]
+        return "".join(result) if result else str(x)
 
 def main():
     # "3の倍数ならFizz、5の倍数ならBuzz、両方とも当てはまるならFizzBuzzを返す
-
-    # fizz = FizzBuzz(3, "Fizz")
-    # buzz = FizzBuzz(5, "Buzz")
-    #
-    # for i in range(1, 31):
-    #     result_string = ""
-    #     result_string += fizz.is_fizzbuzz(i)
-    #     result_string += buzz.is_fizzbuzz(i)
-    #     
-    #     if result_string == "":
-    #         print i
-    #     else:
-    #         print result_string
-
-    fizzbuzz = FizzBuzz({3:"Fizz", 5:"Buzz"});
-    print fizzbuzz.rules
+    fizzbuzz = FizzBuzz({3:"Fizz", 5:"Buzz"})
+    # fizzbuzz.add_rule(2, "Pooh")
     fizzbuzz.show(31)
-    print fizzbuzz.fizzbuzz(6)
-
 
 if __name__ == "__main__":main()
